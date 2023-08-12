6565e504bbb5f9500d8fab480baa628f12252437
oudeis <de@oude.is>
2023-08-12 19:07:53 +0300

    added basic analysis for masternodes

---
+++ analyze.sh
@@ -0,0 +1,26 @@
+function analyze_masternodes() {
+    local json_file="$1"
+
+    # Total number of masternodes
+    echo "Total Masternodes:"
+    jq '.masterNodes | length' "$json_file"
+
+    # Status aggregates
+    echo -e "\nMasternodes by Status:"
+    jq -r '.masterNodes[] | select(.status) | .status' "$json_file" | sort | uniq -c
+
+    # # Aggregates by Payee
+    # echo -e "\nMasternodes by Payee:"
+    # jq -r '.masterNodes[] | select(.payee) | .payee' "$json_file" | sort | uniq -c
+
+    # Analysis: Payees with more than one masternode
+    echo -e "\nPayees with More Than 1 Masternode:"
+    jq -r '.masterNodes[] | select(.payee) | .payee' "$json_file" | sort | uniq -c | sort -nr | awk '$1 > 1'
+
+    # Analysis: List of Masternodes not ENABLED 
+    echo -e "\nList of Masternodes with Status Other Than 'ENABLED':"
+    jq '.masterNodes[] | select(.status != "ENABLED") | {proTxHash, status}' "$json_file"
+}
+
+# Example usage
+# analyze_masternodes "path_to_your_json_file.json"
