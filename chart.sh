f0fd11aed0bf51e42a1ddb0a433f0130943466b2
oudeis <de@oude.is>
2023-08-14 12:36:57 +0300

    charting enabled masternodes instead of total

--- chart.sh
+++ chart.sh
@@ -11,9 +11,10 @@ echo "Extracting data to /tmp/data.txt..."
 git log --pretty=format:'%H|%s' analyzed_stats | head -30 |
 while IFS="|" read commit message; do
     timestamp=$(echo $message | cut -d' ' -f4-8) # Adjusted cut field to omit the day of the week
-    count=$(git diff ${commit}^ $commit -- analyzed_stats | grep -A 1 "Total Masternodes:" | tail -1)
-    count=$(echo $count | tr -d -c "0-9") # this ensures we only take the absolute value of the count
-    echo "$timestamp $count"
+    countTotal=$(git diff ${commit}^ $commit -- analyzed_stats | grep -A 1 "Total Masternodes:" | tail -1)
+    countEnabled=$(git show ${commit}:analyzed_stats | awk '/ENABLED/ {print $1}')
+    countEnabled=$(echo $countEnabled | tr -d -c "0-9") # this ensures we only take the absolute value of the count
+    echo "$timestamp $countEnabled"
 done > /tmp/data.txt
 echo "Data extraction complete. Check /tmp/data.txt."
 
@@ -35,16 +36,14 @@ BEGIN {
 }
 {print $5 "-" MONTH[$1] "-" $2 " " $3, $6}' /tmp/data.txt > /tmp/plot_data.txt
 
-
-
 # Plot the data using gnuplot
 gnuplot -p -e "
     set xdata time;
     set timefmt '%Y-%m-%d %H:%M:%S';
     set format x '%d/%m\n%H:%M';
     set xlabel 'Time';
-    set ylabel 'Masternode count';
-    plot '/tmp/plot_data.txt' using 1:3 with linespoints title 'Masternode count over time';
+    set ylabel 'ENABLED Masternode count';
+    plot '/tmp/plot_data.txt' using 1:3 with linespoints title 'ENABLED Masternode count over time';
 "
 
 # End of the script
