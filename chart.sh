0dd328c7913d4602e69fed92691fd8950ada12ed
oudeis <de@oude.is>
2023-08-14 17:22:23 +0300

    charting total and enabled masternodes

--- chart.sh
+++ chart.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-# TODO:
-# - fix: chart active and total nodes
-# - fix: use commit timestamp instead of commit message
-# - chart addresses wth more than one node -- number of nodes for each
-# - chart added/deleted addresses (think how)
-
 # Extracting data
 echo "Extracting data to /tmp/data.txt..."
 git log --pretty=format:'%H|%s' analyzed_stats | head -30 |
 while IFS="|" read commit message; do
-    timestamp=$(echo $message | cut -d' ' -f4-8) # Adjusted cut field to omit the day of the week
-    countTotal=$(git diff ${commit}^ $commit -- analyzed_stats | grep -A 1 "Total Masternodes:" | tail -1)
-    countEnabled=$(git show ${commit}:analyzed_stats | awk '/ENABLED/ {print $1}')
-    countEnabled=$(echo $countEnabled | tr -d -c "0-9") # this ensures we only take the absolute value of the count
-    echo "$timestamp $countEnabled"
+    timestamp=$(echo $message | cut -d' ' -f4-8) 
+    # echo $timestamp
+    # echo $commit
+    countTotal=$(git show ${commit}:analyzed_stats | awk 'NR==2{print $1}')
+    countEnabled=$(git show ${commit}:analyzed_stats | awk 'NR==5{print $1}')
+    echo "$timestamp $countTotal $countEnabled"
 done > /tmp/data.txt
 echo "Data extraction complete. Check /tmp/data.txt."
+
 
 # Transform the data for proper formatting
 awk '
@@ -34,16 +30,17 @@ BEGIN {
     MONTH["Nov"] = "11";
     MONTH["Dec"] = "12";
 }
-{print $5 "-" MONTH[$1] "-" $2 " " $3, $6}' /tmp/data.txt > /tmp/plot_data.txt
+{print $5 "-" MONTH[$1] "-" $2 " " $3, $6, $7}' /tmp/data.txt > /tmp/plot_data.txt
 
 # Plot the data using gnuplot
 gnuplot -p -e "
     set xdata time;
     set timefmt '%Y-%m-%d %H:%M:%S';
-    set format x '%d/%m\n%H:%M';
+    set format x '%d/%m'; 
     set xlabel 'Time';
-    set ylabel 'ENABLED Masternode count';
-    plot '/tmp/plot_data.txt' using 1:3 with linespoints title 'ENABLED Masternode count over time';
+    set ylabel 'Masternode count';
+    plot '/tmp/plot_data.txt' using 1:3 with linespoints title 'TOTAL Masternode count over time', \
+         '/tmp/plot_data.txt' using 1:4 with linespoints title 'ENABLED Masternode count over time';
 "
 
 # End of the script
